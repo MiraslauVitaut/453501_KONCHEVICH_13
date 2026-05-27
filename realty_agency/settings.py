fe1cee2058e18257c3cd2d240e128967c6c88a56
MiraslauVitaut <mirikjsa@gmail.com>
2026-05-27 22:34:48 +0300

    Fix WhiteNoise middleware for static files

--- realty_agency/settings.py
+++ realty_agency/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -101,7 +102,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise для раздачи статики на Render
-MIDDLEWARE_WHITENOISE = 'whitenoise.middleware.WhiteNoiseMiddleware'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
